dd7bf95860ba0bc1cc01612d3f33919e55c265e5
Holly <papiniu@outlook.com>
2016-11-08 02:02:40 -0800

    Trying to style the front page, missing adventure grid and the journal section

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -2,37 +2,52 @@
 /**
  * The template for displaying all pages.
  *
- * @package RED_Starter_Theme
+ * @package Inhabitent_Theme
  */
 get_header(); ?>
-
-            <div class = "banner-image">
-                     <img src="<?php echo get_template_directory_uri() . '/images/logos/inhabitent-logo-full.svg'; ?>" class="banner-img" alt="inhabitent circle logo" />
-            </div>
-
-            <div id="primary" class="content-area">
-		    <main id="main" class="site-main" role="main">
-
-            <?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-			<?php endwhile; // End of the loop. ?>
-
-            </main>
-            
-            <section class = "shop-stuff">
-                Shop stuff
+    <div class="banner-image">
+        <img src="<?php echo get_template_directory_uri() . '/images/logos/inhabitent-logo-full.svg'; ?>" class="banner-img" alt="inhabitent circle logo"
+        />
+    </div>
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <!--Shop place -->
+            <section class="shop">
+                <h1> Shop stuff </h1>
+                <div class="shop-section">
+                    <!--Do seciton of shop-->
+                    <div class="shop-promotion">
+                        <img src="<?php echo get_template_directory_uri() . '/images/product-type-icons/do.svg'; ?>" alt="Do Icon" />
+                        <p>Get back to nature with all the tools and toys you need to enjoy the great outdoors </p>
+                        <a href="#" class="button"> do stuff </a>
+                    </div>
+                    <!--Eat section of shop-->
+                    <div class="shop-promotion">
+                        <img src="<?php echo get_template_directory_uri() . '/images/product-type-icons/eat.svg'; ?>" alt="Eat Icon" />
+                        <p>Get back to nature with all the tools and toys you need to enjoy the great outdoors </p>
+                        <a href="#" class="button"> eat stuff </a>
+                    </div>
+                    <!--Sleep section of shop-->
+                    <div class="shop-promotion">
+                        <img src="<?php echo get_template_directory_uri() . '/images/product-type-icons/sleep.svg'; ?>" alt="Sleep Icon" />
+                        <p>Get back to nature with all the tools and toys you need to enjoy the great outdoors </p>
+                        <a href="#" class="button"> sleep stuff </a>
+                    </div>
+                    <!--Wear section of shop-->
+                    <div class="shop-promotion">
+                        <img src="<?php echo get_template_directory_uri() . '/images/product-type-icons/wear.svg'; ?>" alt="Wear Icon" />
+                        <p>Get back to nature with all the tools and toys you need to enjoy the great outdoors
+                        </p>
+                        <a href="#" class="button"> wear stuff </a>
+                    </div>
+                </div>
             </section>
-
-            <section class = "journal">
-               
+            <!-- shopping section ends -->
+            <!--adventure section -->
+            <section class="adventures">
+                <h1>latest adventures</h1>
             </section>
-
-            <section class = "adventures">
-            </section>
-			        
-	</div><!-- #secondary -->
-
-    </body> 
-
-
-<?php get_footer(); ?>
+    </div>
+    <!-- #secondary -->
+    </body>
+    <?php get_footer(); ?>
