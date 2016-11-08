0788b556660359321362f75b2a65378ea87b546c
Holly <papiniu@outlook.com>
2016-11-07 16:18:06 -0800

    Added front page, also added a new plug in

---
+++ themes/inhabitent/front-page.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+get_header(); ?>
+            <section class = "banner-image">
+                     <img src="<?php echo get_template_directory_uri() . '/images/logos/inhabitent-logo-full.svg'; ?>" class="banner-img" alt="inhabitent circle logo" />
+            </section>
+
+            <div id="primary" class="content-area">
+		    <main id="main" class="site-main" role="main">
+            <section class = "shop-stuff">
+                Shop stuff
+            </section>
+
+            <section class = "journal">
+               
+            </section>
+
+            <section class = "adventures">
+            </section>
+			        
+	</div><!-- #secondary -->
+
+    </body> 
+
+
+<?php get_footer(); ?>
