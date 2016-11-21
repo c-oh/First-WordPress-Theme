9f7c9d04c1773326851754d3af3598815e760457
Holly <papiniu@outlook.com>
2016-11-21 13:12:44 -0800

    Added the necessary files to style products page

--- themes/inhabitent/page-templates/archive-product.php
+++ themes/inhabitent/page-templates/archive-product.php
@@ -1,44 +1,55 @@
 <?php
 /**
- * The template for displaying archive pages.
- *
- * @package RED_Starter_Theme
- */
-
+* The template for displaying archive pages.
+*
+* @package Inhabitent_Theme
+*/
 get_header(); ?>
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-			<?php if ( have_posts() ) : ?>
-			<header class="archive">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-				?>
-			</header>
-			<!-- .page-header -->
-			<?php $terms = get_terms('product_type'); ?> 
-			<?php foreach ($terms as $term) : ?> 
-			<div class='shop-stuff'>
-				<?php $url = get_term_link ($term->slug , 'product_type'); ?> 
-				<a href='<?php echo $url ?>' class='button'>
-					<?php  echo $term->name ?>
-				</a>
+		<main id="main" class="site-main">
+			<div class="container">
+				<?php if ( have_posts() ) : ?>
+				<header class="page-header">
+					<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+					<ul>
+						<?php
+								$terms = get_terms('product_type');
+								foreach ($terms as $term) :
+									?>
+							<li>
+								<?php $url = get_term_link($term->slug, 'product_type'); ?>
+								<p><a href="<?php echo $url ?>"><?php echo $term->name ?></a></p>
+							</li>
+							<?php endforeach;?>
+					</ul>
+				</header>
+				<!-- .page-header -->
+				<div class="product-grid">
+					<ul>
+						<?php while ( have_posts() ) : the_post(); ?>
+						<li>
+							<div class="thumbnail-wrap">
+								<a href="<?php the_permalink(); ?>">
+									<?php the_post_thumbnail('large'); ?>
+								</a>
+							</div>
+							<div class="product-wrap">
+								<h2><span><?php the_title(); ?></span></h2>
+								<p>
+									<?php echo CFS()->get( 'product_price' ); ?>
+								</p>
+							</div>
+						</li>
+						<?php endwhile; ?>
+						<?php else : ?>
+						<?php get_template_part( 'template-parts/content', 'none' ); ?>
+						<?php endif; ?>
+					</ul>
+				</div>
 			</div>
-			
-			<?php endforeach; ?>
-			<div id="primary" class="archive-area">
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
-					<?php endwhile; ?>
-					<?php the_posts_navigation(); ?>
-					<?php else : ?>
-					<?php get_template_part( 'template-parts/content', 'none' ); ?>
-					<?php endif; ?>
+			<!--.container-->
 		</main>
 		<!-- #main -->
-		</div>
-		<!-- #primary -->
-		<?php get_sidebar(); ?>
-		<?php get_footer(); ?>
+	</div>
+	<!-- #primary -->
+	<?php get_footer(); ?>
--- themes/inhabitent/page-templates/single-product.php
+++ themes/inhabitent/page-templates/single-product.php
@@ -1,32 +1,38 @@
 <?php
 /**
- * The template for displaying all single posts.
- *
- * @package RED_Starter_Theme
- */
-
+* The template for displaying all single posts.
+*
+* @package Inhabitent_Theme
+*/
 get_header(); ?>
-<h1><?php echo CFS()->get( 'product_price' ); ?></h1>
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php while ( have_posts() ) : the_post(); ?>
-			
-
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
-
-			<?php the_post_navigation(); ?>
-
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
-
-		<?php endwhile; // End of the loop. ?>
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+		<main id="main" class="site-main">
+			<article class="one-product container">
+				<?php while ( have_posts() ) : the_post(); ?>
+				<div class="product-img">
+					<?php the_post_thumbnail( 'large' ); ?>
+				</div>
+				<div class="product-desc">
+					<h1>
+						<?php the_title(); ?>
+					</h1>
+					<h2>
+						<?php echo CFS()->get( 'product_price' ); ?>
+					</h2>
+					<?php the_content(); ?>
+					<div class="social-wrap">
+						<p class="white-link"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i> Like</a></p>
+						<p class="white-link"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i> Tweet</a></p>
+						<p class="white-link"><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i> Pin</a></p>
+					</div>
+					<!--.social-wrap-->
+				</div>
+				<!--.product-desc-->
+				<?php endwhile; // End of the loop. ?>
+			</article>
+			<!--.container-->
+		</main>
+		<!-- #main -->
+	</div>
+	<!-- #primary -->
+	<?php get_footer(); ?>
