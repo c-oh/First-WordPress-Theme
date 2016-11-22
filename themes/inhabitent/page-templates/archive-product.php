31797e2205f4102e94ea9c18ca3cff460f6a7d56
Holly <papiniu@outlook.com>
2016-11-21 17:25:57 -0800

    Trying to style the shop page

--- themes/inhabitent/page-templates/archive-product.php
+++ themes/inhabitent/page-templates/archive-product.php
@@ -1,55 +1,49 @@
 <?php
 /**
-* The template for displaying archive pages.
-*
-* @package Inhabitent_Theme
-*/
+ * The template for displaying archive pages.
+ *
+ * @package Inhabitent_Theme
+ */
 get_header(); ?>
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<div class="shop-area">
+		<main id="main" class="site-main" role="main">
 			<div class="container">
 				<?php if ( have_posts() ) : ?>
-				<header class="page-header">
-					<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
-					<ul>
-						<?php
-								$terms = get_terms('product_type');
-								foreach ($terms as $term) :
-									?>
-							<li>
-								<?php $url = get_term_link($term->slug, 'product_type'); ?>
-								<p><a href="<?php echo $url ?>"><?php echo $term->name ?></a></p>
-							</li>
-							<?php endforeach;?>
-					</ul>
+				<header class="archive-title">
+					<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+					?>
+			
+			<!-- .page-header -->
+					<div class="shop-stuff-list">
+						<?php $terms = get_terms('product_type'); ?>
+						<?php foreach ($terms as $term) : ?> 
+						<div class='shop-stuff'>
+							<?php $url = get_term_link ($term->slug , 'product_type'); ?>
+							<a href='<?php echo $url ?>' class='button'>
+								<?php  echo $term->name ?>
+							</a>
+						</div> <!-- shop stuff -->
+						<?php endforeach; ?>
+					</div> <!--  shop stuff list -->
 				</header>
-				<!-- .page-header -->
-				<div class="product-grid">
-					<ul>
-						<?php while ( have_posts() ) : the_post(); ?>
-						<li>
-							<div class="thumbnail-wrap">
-								<a href="<?php the_permalink(); ?>">
-									<?php the_post_thumbnail('large'); ?>
-								</a>
-							</div>
-							<div class="product-wrap">
-								<h2><span><?php the_title(); ?></span></h2>
-								<p>
-									<?php echo CFS()->get( 'product_price' ); ?>
-								</p>
-							</div>
-						</li>
-						<?php endwhile; ?>
-						<?php else : ?>
-						<?php get_template_part( 'template-parts/content', 'none' ); ?>
-						<?php endif; ?>
-					</ul>
-				</div>
-			</div>
-			<!--.container-->
+
+				<div id="primary" class="archive-area">
+					<?php /* Start the Loop */ ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+					<div class="product-item">
+						<?php get_template_part( 'template-parts/product-archive' ); ?>
+					</div>
+					<?php endwhile; ?>
+				</div> <!-- archive area -->
+					<?php the_posts_navigation(); ?>
+					<?php else : ?>
+					<?php get_template_part( 'template-parts/product-archive', 'none' ); ?>
+				
+					<?php endif; ?>
+			</div> <!-- container -->
 		</main>
 		<!-- #main -->
-	</div>
-	<!-- #primary -->
-	<?php get_footer(); ?>
+	</div> <!-- content-area -->
+
+<?php get_footer(); ?>
