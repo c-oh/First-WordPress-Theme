bc545771607205bbad8ca592a9ae37af033d536f
Holly <papiniu@outlook.com>
2016-11-23 15:45:13 -0800

    dying

--- themes/inhabitent/js/scripts.js
+++ themes/inhabitent/js/scripts.js
@@ -1,12 +1,14 @@
+
 (function( $ ) {
- //shows the search input field on click 
- $('.icon-search').on('click', function(event){
-   event.preventDefault();
-   $('.header-search .search-field').animate({width: 200}).focus();
- });
- //hides the search input field when not in focus
- $('.header-search .search-field').on('focusout', function(){
-   $(this).animate({width: 0});
- });
- 
+  //shows the search input field on click 
+  $('.icon-search').on('click', function(event){
+    event.preventDefault();
+    $('.header-search .search-field').animate({width: 200}).focus();
+  });
+  //hides the search input field when not in focus
+  $('.header-search .search-field').on('focusout', function(){
+    $(this).animate({width: 0});
+  });
+       
+
 })( jQuery );
--- themes/inhabitent/single.php
+++ themes/inhabitent/single.php
@@ -13,24 +13,24 @@ get_header(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
-			<!-- previous and next post navigation >-->
-			<!--<?php the_post_navigation(); ?>-->
+		
+		<?php the_post_navigation(); ?>
 
 			
 
-			</div> <!--social buttons -->
+			</div> 
 
 			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
+				
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
 			?>
 
-		<?php endwhile; // End of the loop. ?>
+		<?php endwhile;  ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</main>
+	</div>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
