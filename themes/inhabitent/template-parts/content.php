557cb42eff1360a1a6dcc1fa5aed8cc3aa051c39
Holly <papiniu@outlook.com>
2016-11-23 23:08:38 -0800

    Finish styling archive page, messed up single journal page

--- themes/inhabitent/template-parts/content.php
+++ themes/inhabitent/template-parts/content.php
@@ -4,7 +4,6 @@
  *
  * @package Inhabitent_Theme
  */
-
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -16,7 +15,9 @@
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
-		
+		<div class="entry-meta">
+			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
+		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
